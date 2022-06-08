72f8cb684e8e41b72362268528a44285d37c3891
NikosGialitsis <nikolasyal@gmail.com>
2022-06-08 04:21:56 +0300

    Update C_Interface.h

--- c_wrapper/C_Interface.h
+++ c_wrapper/C_Interface.h
@@ -9,7 +9,7 @@ extern "C" {
 // static const char* objects_storage_file = "/home/ngialitsis/search/dmat/dmat_20news_raw_n5_w5_mincs.bin";
 //static const char* objects_storage_file = "/home/ngialitsis/search/dmat/dmat_20news_raw_n5_w5_sqrt_cs.bin";
 
-static const char* objects_storage_file = "/storage4/ngialitsis_home/search/dmat/dmat_20news_raw_n5_w5_vs.bin";
+static const char* objects_storage_file = "/storage4/ngialitsis_home/search/dmat/dmat_20news_raw_n5_w5_trigen_vs.bin";
 
 // static const char* objects_storage_file = "/home/ngialitsis/search/dmat/dmat_20news_raw_n5_w5_mincs.bin";
 
